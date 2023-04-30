cdbc9b58471f62396fd09611b2d03ed727b2c88c
gaurabwebdev <gaurabbose40@gmail.com>
2023-04-30 16:17:12 +0600

    all done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const newsCategories = require("./data/categories.json");
+const news = require("./data/news.json");
 const cors = require("cors");
 
 app.use(cors());
@@ -10,8 +11,30 @@ app.get("/", (req, res) => {
   res.send("Hello News Dragon.");
 });
 
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
 app.get("/categories", (req, res) => {
   res.send(newsCategories);
+});
+
+app.get("/news/:newsId", (req, res) => {
+  const currentNewsId = req.params.newsId;
+  const currentNews = news.find((n) => n._id === currentNewsId);
+  res.send(currentNews);
+});
+
+app.get("/categories/:categoryId", (req, res) => {
+  const newsCategoryId = req.params.categoryId;
+  if (newsCategoryId === "0") {
+    res.send(news);
+  } else {
+    const currentCategoryNews = news.filter(
+      (n) => n.category_id === newsCategoryId
+    );
+    res.send(currentCategoryNews);
+  }
 });
 
 app.listen(port, () => {
